2b8e723db8d66336d68b78d5dc0bf9cc038d312e
darinlarimore <darinlarimore@gmail.com>
2024-09-30 13:46:26 -0400

    remove comment

--- src/Console/Commands/GeneratorCommand.php
+++ src/Console/Commands/GeneratorCommand.php
@@ -6,12 +6,6 @@ use Statamic\Console\Commands\GeneratorCommand as StatamicGeneratorCommand;
 
 class GeneratorCommand extends StatamicGeneratorCommand
 {
-    /**
-     * Taken from Simple Commerce
-     *
-     * We need to do this ourselves so it uses the
-     * Simple Commerce Australia Post stub path.
-     */
     protected function getStub($stub = null): string
     {
         $stub = $stub ?? $this->stub;
