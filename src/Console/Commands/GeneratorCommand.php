e334a450ba50b189ac722ef589be1bfc04fa6531
darinlarimore <darinlarimore@gmail.com>
2024-09-30 16:38:37 -0400

    added ups shipping name

--- src/Console/Commands/GeneratorCommand.php
+++ src/Console/Commands/GeneratorCommand.php
@@ -4,12 +4,14 @@ namespace Darinlarimore\SimpleCommerceUps\Console\Commands;
 
 use Statamic\Console\Commands\GeneratorCommand as StatamicGeneratorCommand;
 
-class GeneratorCommand extends StatamicGeneratorCommand
-{
+class GeneratorCommand extends StatamicGeneratorCommand{
+
     protected function getStub($stub = null): string
     {
         $stub = $stub ?? $this->stub;
 
         return __DIR__.'/stubs/'.$stub;
     }
+
+    protected $name = "ups-shipping";
 }
