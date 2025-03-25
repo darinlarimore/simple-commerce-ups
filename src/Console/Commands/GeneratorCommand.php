b9bbd2d863d43207a8c6ec8741f36549dbe8f9cb
darinlarimore <darinlarimore@gmail.com>
2025-03-24 20:33:52 -0400

    added name for generatorcommand

--- src/Console/Commands/GeneratorCommand.php
+++ src/Console/Commands/GeneratorCommand.php
@@ -6,6 +6,8 @@ use Statamic\Console\Commands\GeneratorCommand as StatamicGeneratorCommand;
 
 class GeneratorCommand extends StatamicGeneratorCommand{
 
+    protected $name = 'simple-commerce-ups:generate';
+
     protected function getStub($stub = null): string
     {
         $stub = $stub ?? $this->stub;
